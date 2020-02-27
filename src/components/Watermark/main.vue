fb7c81f883a494310b25ffa7b0998954fd9b13d6
xuanmos <me@xuanmo.xin>
2020-02-27 09:11:30 +0800

    update

--- src/components/Watermark/main.vue
+++ src/components/Watermark/main.vue
@@ -33,6 +33,14 @@ export default {
     opacity: {
       type: Number,
       default: 0.1
+    },
+    width: {
+      type: [Number, String],
+      default: '100%'
+    },
+    height: {
+      type: [Number, String],
+      default: '100%'
     }
   },
   data () {
@@ -42,11 +50,17 @@ export default {
       style: {}
     }
   },
+  watch: {
+    height (height) {
+      this.style.height = height
+      this.init()
+    }
+  },
   created () {
     this.style = Object.assign({}, this.style, {
       opacity: this.opacity,
-      width: '100%',
-      height: '100%',
+      width: this.width,
+      height: this.height,
       'pointer-events': 'none'
     })
   },
@@ -55,18 +69,21 @@ export default {
   },
   methods: {
     init () {
-      const canvas = this.$refs.canvas
-      const ctx = canvas.getContext('2d')
-      const parentWidth = this.$refs.wrapper.offsetWidth
-      const parentHeight = this.$refs.wrapper.offsetHeight
-      canvas.width = parentWidth * this.devicePxRatio
-      canvas.height = parentHeight * this.devicePxRatio
+      this.img = ''
+      this.$nextTick(() => {
+        const canvas = this.$refs.canvas
+        const ctx = canvas.getContext('2d')
+        const parentWidth = this.$refs.wrapper.offsetWidth
+        const parentHeight = this.$refs.wrapper.offsetHeight
+        canvas.width = parentWidth * this.devicePxRatio
+        canvas.height = parentHeight * this.devicePxRatio
 
-      // 重复绘制,详细说明：https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasRenderingContext2D/createPattern
-      const pattern = ctx.createPattern(this.createSmallMark(), 'repeat')
-      ctx.fillStyle = pattern
-      ctx.fillRect(0, 0, parentWidth * this.devicePxRatio, parentHeight * this.devicePxRatio)
-      this.img = canvas.toDataURL('image/png', 1)
+        // 重复绘制,详细说明：https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasRenderingContext2D/createPattern
+        const pattern = ctx.createPattern(this.createSmallMark(), 'repeat')
+        ctx.fillStyle = pattern
+        ctx.fillRect(0, 0, parentWidth * this.devicePxRatio, parentHeight * this.devicePxRatio)
+        this.img = canvas.toDataURL('image/png', 1)
+      })
     },
 
     // 创建水印小图
@@ -78,27 +95,42 @@ export default {
       // 获取文字宽度用于计算canvas宽高
       const { width: textWidth } = ctx.measureText(this.text)
 
+      const maxWidth = Math.sqrt(textWidth ** 2 + this.fontStyle.size ** 2)
+
       // 宽度 = 文字宽度 * 设备像素比，防止模糊
-      canvas.width = textWidth * this.devicePxRatio
+      canvas.width = maxWidth * this.devicePxRatio
 
       // 高度同宽度计算一样
-      canvas.height = textWidth * this.devicePxRatio
+      canvas.height = maxWidth * this.devicePxRatio
 
       // 设置canvas style
-      canvas.style = `width: ${textWidth}px; height: ${textWidth}px`
-
-      ctx.translate(canvas.width / 2, canvas.height / 2 - this.fontStyle.size / 2)
-
-      // 文字x轴对齐方式
-      ctx.textAlign = 'left'
-
-      // 文字y轴对齐方式
-      ctx.textBaseline = 'bottom'
+      canvas.style = `width: ${maxWidth}px; height: ${maxWidth}px`
 
       ctx.font = `${this.fontStyle.size * 2}px ${this.fontStyle.family}`
+
       ctx.fillStyle = this.fontStyle.color
+
       ctx.rotate(this.rotate * Math.PI / 180)
-      ctx.fillText(this.text, canvas.width / -2, -this.rotate)
+
+      if (this.rotate > 0) {
+        ctx.translate(this.rotate / 2, 0)
+
+        // 文字x轴对齐方式
+        ctx.textAlign = 'left'
+
+        // 文字y轴对齐方式
+        ctx.textBaseline = 'top'
+        ctx.fillText(this.text, 0, 0)
+      } else {
+        ctx.translate(this.rotate * 2, 0)
+
+        // 文字x轴对齐方式
+        ctx.textAlign = 'left'
+
+        // 文字y轴对齐方式
+        ctx.textBaseline = 'bottom'
+        ctx.fillText(this.text, 0, canvas.height)
+      }
       return canvas
     }
   }
